befe36b3f94dab2a92db9b3641635651b1528f99
alejandroPolak <alejandro_polak@hotmail.com>
2020-12-04 00:51:53 -0300

    Face detected funcionando Sin base de datos

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
-    return(
-        <div>
-            <p className= 'f3' style={{textAlign: 'center'}}>
-            {'This Magic Brain will detecter faces in you picture. Git it try.'}
-            </p>
-                <div className='center'>
-                    <div className= 'form center pa4 br3 shadow-5 '>
-                        <input className='f4 pa2 w-70' type='text'/>
-                        <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue dark-blue'>Detect</button>
-
-                    </div>
-                </div>
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
+  return (
+    <div>
+      <p className='f3' style={{textAlign: 'center'}}>
+        {'This Magic Brain will detecter faces in you picture. Git it try.'}
+      </p>
+      <div className='center'>
+        <div className='form center pa4 br3 shadow-5 '>
+          <input className='f4 pa2 w-70' type='text' onChange={onInputChange} />
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue dark-blue'
+            onClick={onButtonSubmit}
+          >
+            Detect
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-
-export default ImageLinkForm;
+export default ImageLinkForm
