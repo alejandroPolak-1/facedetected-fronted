6c88142a3ab34ab0b77ac6dec7bf1ba494696d1a
alejandroPolak <alejandro_polak@hotmail.com>
2020-12-09 12:06:29 -0300

    rank solucionado problema consulta a api backend

--- src/conteiners/App.js
+++ src/conteiners/App.js
@@ -90,7 +90,7 @@ class App extends Component {
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then((response) => {
         if (response) {
-          fetch(`${process.env.REACT_APP_API_URL}/image`, {
+          fetch(`${process.env.REACT_APP_API_URL}image`, {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
